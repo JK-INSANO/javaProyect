367cf4838a145c1a2f4dc4fc2895d0af3061ddab
broken_Arm <brunotoro1799@gmail.com>
2023-09-04 11:18:14 -0300

    agregue comentarios

--- beta01/src/ventanas/MostarArchivo.java
+++ beta01/src/ventanas/MostarArchivo.java
@@ -5,7 +5,7 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-
+//hola
 public class MostarArchivo extends JFrame {
 
 	private JPanel contentPane;
